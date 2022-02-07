84f7837670e1bf61ab982615fc292aed30ce096c
Kevin <dbsgur46894689@gmail.com>
2022-02-07 15:15:07 +0900

    20220207 Make Cards

--- client/src/components/views/LandingPage/LandingPage.js
+++ client/src/components/views/LandingPage/LandingPage.js
@@ -1,19 +1,60 @@
 import axios from "axios";
-
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
+import { Icon, Col, Card, Row } from "antd";
+import Meta from "antd/lib/card/Meta";
 
 function LandingPage() {
+  const [products, setProducts] = useState([]);
+
   useEffect(() => {
     axios.get("/api/product/products").then((response) => {
       if (response.data.success) {
         console.log(response.data);
+        setProducts(response.data.productInfo);
       } else {
         alert("상품을 가져오는데 실패하였습니다");
       }
     });
   }, []);
 
-  return <div>LandingPage</div>;
+  const renderCards = products.map((product, index) => {
+    console.log(product, index);
+    return (
+      <Col key={index} lg={6} md={8} xs={24}>
+        <Card
+          cover={
+            <img
+              style={{ width: "100%", maxHeight: "150px" }}
+              src={`http://localhost:4000/${product.images[0]}`}
+            />
+          }
+        >
+          <Meta title={product.title} description={`$${product.price}`} />
+        </Card>
+      </Col>
+    );
+  });
+
+  return (
+    <div style={{ width: "75%", margin: "3rem auto" }}>
+      <div style={{ textAlign: "center" }}>
+        <h2>
+          Let's Travel Anywhere
+          <Icon type="rocket" />
+        </h2>
+      </div>
+      {/* Filter */}
+
+      {/* Search */}
+
+      {/* Cards */}
+      <Row gutter={[16, 16]}>{renderCards}</Row>
+
+      <div style={{ display: "flex", justifyContent: "center" }}>
+        <button>더보기</button>
+      </div>
+    </div>
+  );
 }
 
 export default LandingPage;
